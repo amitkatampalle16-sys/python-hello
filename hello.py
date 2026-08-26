db7d73d71d7558fc4a404c57d40d142f07161ba0
Amit Katampalle <amitkatampalle16@gmail.com>
2026-08-26 13:52:05 +0530

    update6 hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,4 @@ print("Hello Amit! Python is working successfully in your jenkins")
 print("Hello Amit! any problem ") 
 print(" any time") 
 print("Hello Amit! any problem 
+      print("Hello Amit! any problem ") 
